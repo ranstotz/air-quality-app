cdecde513ac27d37129decbc22427687f5b1ab5c
Ryan Anstotz <ryananstotz@gmail.com>
2020-04-15 21:45:44 -0400

    modify flask to return correct index and api routes.

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
+from flask import jsonify, make_response
 import os
-from flask import Flask, jsonify, render_template
+from flask import Flask, jsonify, render_template, send_from_directory
+
+from server.covid_data import get_state_data, get_country_data
 
 app = Flask(__name__, static_folder='./client/dist',
             template_folder='./client/dist')
@@ -8,15 +11,22 @@ app = Flask(__name__, static_folder='./client/dist',
 @app.route('/api/items')
 def items():
     '''Sample API route for data'''
-    return jsonify([{'title': 'A'}, {'title': 'B'}])
+    # return jsonify([{'title': 'A'}, {'title': 'B'}])
+    return jsonify({'title': 'A'})
 
 # Serve React App
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
 def serve(path):
     ''' serve index.html for non-api routes '''
-    return render_template("index.html")
+    # if statement needed to accurately serve supporting files to index.html
+    if path != "" and os.path.exists(app.static_folder + '/' + path):
+        return send_from_directory(app.static_folder, path)
+    else:
+        # return send_from_directory(app.static_folder, 'index.html')
+        return render_template("index.html")
 
 
 if __name__ == '__main__':
     app.run(use_reloader=True, port=5000, threaded=True)
+    
